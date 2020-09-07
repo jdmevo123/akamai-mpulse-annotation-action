ca1ba72b1a848fe551e99e1cdba02e5840bc2882
jdmevo123 <dale_lenard@hotmail.com>
2020-09-07 19:53:09 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,47 +3,65 @@ set -e
 set -o pipefail
 
 # Set Variables
-apiToken="4b87edd6-1123-471d-9e77-a8b8991ca9f3"
-title="test" #$4
-start="1599439052853" #$5
-text="TestingAction" #$6
-#domainIds="" #$7
-executionType="" #$8
+apiToken="4b87edd6-1123-471d-9e77-a8b8991ca9f3" #$1
+title="test" #$2
+time="1599439052853" #$3
+text="TestingAction" #$4
+domainIds="" #$5
+annotationID = "" #$6
 
-# # Get API Token
-# curl -X PUT -H "Content-type: application/json" --data-binary '{"userName":${userName}, "password":${password}}' \
-#   "https://mpulse.soasta.com/concerto/services/rest/RepositoryService/v1/Tokens"
-# Auth-Token=
-# example response = {"token":"da7be4d72030656559f3e41a98924a6b2a544730"}
-# curl -X PUT -H "Content-type: application/json" --data-binary '{"apiToken":"'${apiToken}'"}' \
-#   "https://mpulse.soasta.com/concerto/services/rest/RepositoryService/v1/Tokens" | jq '.["token"]'
 # Get API Token SSO
 authToken=$(curl -X PUT -H "Content-type: application/json" --data-binary '{"apiToken":"'${apiToken}'"}' \
   "https://mpulse.soasta.com/concerto/services/rest/RepositoryService/v1/Tokens" | jq '.["token"]' | tr -d '"')
 echo $authToken
-# Execute Annotation
-if [ -z "$domainIds" ]; then
-   data='{ "title":"'${title}'", "start":'${start}', "text":"'${text}'" }'
+if [ "$annotationID" == "" ]; then
+    # Execute Annotation
+    if [ "$domainIds" == "" ]; then
+       data='{ "title":"'${title}'", "start":'${time}', "text":"'${text}'" }'
+    fi
+    if [ "$domainIds" != "" ]; then
+       data='{ "title":"'${title}'", "start":'${time}', "text":"'${text}'", "domainIds":"['${domainIds}']" }'
+    fi
+    annotationID=$(curl -X POST \
+         -H "X-Auth-Token: $authToken" \
+         -H "Content-type: application/json" \
+         --data-binary "$data"\
+         "https://mpulse.soasta.com/concerto/mpulse/api/annotations/v1")
+    echo $annotationID
+    annotationID=$(echo $annotationID | jq '.["id"]' | tr -d '"')
+    echo $annotationID
+    if [ "$annotationID" == null ]; then
+      echo "Failed : Annotation ID was empty" && exit 1;
+    fi
+    if [ -n "$annotationID" ]; then
+      echo "success : $annotationID"
+      echo $annotationID > annotationID.txt
+    fi
+    if [ -z "$annotationID" ]; then
+      echo "Failed : $annotationID" && exit 1;
+    fi
 fi
-if [ -n "$domainIds" ]; then
-   data='{ "title":"'${title}'", "start":'${start}', "text":"'${text}'", "domainIds":"['${domainIds}']" }'
-fi
-# echo '{"title":"'$title'","start":'$start',"text":"'$text'"}'
-annotationID=$(curl -X POST \
-     -H "X-Auth-Token: $authToken" \
-     -H "Content-type: application/json" \
-     --data-binary "$data"\
-     "https://mpulse.soasta.com/concerto/mpulse/api/annotations/v1")
-echo $annotationID
-annotationID=$(echo $annotationID | jq '.["id"]' | tr -d '"')
-echo $annotationID
-if [ "$annotationID" == null ]; then
-  echo "Failed : Annotation ID was empty" && exit 1;
-fi
-if [ -n "$annotationID" ]; then
-  echo "success : $annotationID"
-  echo $annotationID > annotationID.txt
-fi
-if [ -z "$annotationID" ]; then
-  echo "Failed : $annotationID" && exit 1;
+
+# Execute append Annotation with end time
+if [ "annotationID" != "" ]; then
+    # Execute Annotation Update
+    data='{ "end":'${time}' }'
+    ammend=$(curl -X POST \
+         -H "X-Auth-Token: $authToken" \
+         -H "Content-type: application/json" \
+         --data-binary "$data"\
+         "https://mpulse.soasta.com/concerto/mpulse/api/annotations/v1/$annotationID")
+    echo $ammend
+    ammend=$(echo $annotationID | jq '.["id"]' | tr -d '"')
+    echo $ammend
+    if [ "$ammend" == null ]; then
+      echo "Failed : Annotation ID was empty" && exit 1;
+    fi
+    if [ "$ammend" == "$annotationID" ]; then
+      echo "success : $ammend"
+      echo "Annotation ID updated with end time"
+    fi
+    if [ -z "$ammend" ]; then
+      echo "Failed : $ammend" && exit 1;
+    fi
 fi
